11b8e284c9eebf8c52d59771646e329b583b3945
David Olson <davidalolson>
2024-04-25 14:38:29 -0600

    Complete core requirements one and two. Going to mod code for three

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Please enter your grade percentage: ");
+        string rawGrade = Console.ReadLine();
+        
+        int grade = int.Parse(rawGrade);
+
+        if (grade >= 90)
+        {
+            Console.WriteLine("You earned an A");
+        }
+        else if (grade < 90 && grade >= 80)
+        {
+            Console.WriteLine("You earned a B");
+        }
+        else if (grade < 80 && grade >=70)
+        {
+            Console.WriteLine("You earned a C");
+        }
+        else if (grade < 70 && grade >= 60)
+        {
+            Console.WriteLine("You earned(?) a D");
+        }
+        else
+        {
+            Console.WriteLine("You got an F");
+        }
+        if (grade < 70)
+        {
+            Console.WriteLine("Hrm, looks like you failed. Encouraging words.");
+        }
+        else
+        {
+            Console.WriteLine("A WINNER IS YOU");  // Pro Wrestling NES 1986
+        }
+
     }
 }
