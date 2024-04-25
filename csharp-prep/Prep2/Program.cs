de26412a8a559eb7ca5134928471dd84df919174
David Olson <davidalolson>
2024-04-25 15:07:51 -0600

    Completed remainder of requirements and streach

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,42 +1,91 @@
 using System;
 
-class Program
+string letter; // set up letter variable (w/o init)
+string sign = ""; //set up sign variable (init to "")
+
+Console.Write("Please enter your grade percentage: ");
+string rawGrade = Console.ReadLine();
+
+int grade = int.Parse(rawGrade);
+
+if (grade >= 90)
 {
-    static void Main(string[] args)
+    letter = "A";
+    if (grade > 100)
     {
-        Console.Write("Please enter your grade percentage: ");
-        string rawGrade = Console.ReadLine();
-        
-        int grade = int.Parse(rawGrade);
-
-        if (grade >= 90)
-        {
-            Console.WriteLine("You earned an A");
-        }
-        else if (grade < 90 && grade >= 80)
-        {
-            Console.WriteLine("You earned a B");
-        }
-        else if (grade < 80 && grade >=70)
-        {
-            Console.WriteLine("You earned a C");
-        }
-        else if (grade < 70 && grade >= 60)
-        {
-            Console.WriteLine("You earned(?) a D");
-        }
-        else
-        {
-            Console.WriteLine("You got an F");
-        }
-        if (grade < 70)
-        {
-            Console.WriteLine("Hrm, looks like you failed. Encouraging words.");
-        }
-        else
-        {
-            Console.WriteLine("A WINNER IS YOU");  // Pro Wrestling NES 1986
-        }
-
+        sign = "+"; // precaution for crazy high grades (lots of extra credit?)
     }
+    else
+    {
+        if (grade - 90 >= 7)
+        {
+            sign = "+";
+        }
+        else if (grade - 90 < 3 && grade! >= 100)
+        {
+            sign = "-";
+        }
+    }
+}
+else if (grade < 90 && grade >= 80)
+{
+    letter = "B";
+    if (grade - 80 >= 7)
+    {
+        sign = "+";
+    }
+    else if (grade - 80 < 3)
+    {
+        sign = "-";
+    }
+}
+else if (grade < 80 && grade >= 70)
+{
+    letter = "C";
+    if (grade - 70 >= 7)
+    {
+        sign = "+";
+    }
+    else if (grade - 70 < 3)
+    {
+        sign = "-";
+    }
+}
+else if (grade < 70 && grade >= 60)
+{
+    letter = "D";
+    if (grade - 60 >= 7)
+    {
+        sign = "+";
+    }
+    else if (grade - 60 < 3)
+    {
+        sign = "-";
+    }
+}
+else
+{
+    letter = "F"; // Defaults to F
+
+    //"...recognize that there is no F+ or F- grades, only F. 
+    //Add additional logic to your program to detect these 
+    //cases and handle them correctly."
+    
+    //Problem is there is no such thing as F+ or F- 
+    //and no such system
+    if (letter == "F")
+    {
+        letter = "F"; // logic handling case correctly
+    }
+}
+
+Console.WriteLine($"You recieved: {letter}{sign}");
+
+if (grade < 70)
+{
+    Console.WriteLine("Hrm, looks like you failed. Encouraging words.");
+}
+else
+{
+    Console.WriteLine("A WINNER IS YOU");  // Pro Wrestling NES 1986
 }
