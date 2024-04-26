8be7e538907175698c6d8232d2f40d7ed4359abb
David Olson <davidalolson>
2024-04-26 17:26:14 -0600

    Completed all requirements

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,54 @@
 using System;
+using System.Dynamic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("");
+        // IO
+        DisplayWelcome();
+
+        string getName = PromptUserName();
+        
+        int getNumber = PromptUserNumber();
+        
+        int getSquare = SquareNumber(getNumber);
+        
+        DisplayResult(getName, getSquare);
+
+
+        // Functions
+        static void DisplayWelcome()
+        {
+            Console.WriteLine("Welcome to the program!");
+        }
+
+        static string PromptUserName()
+        {
+            Console.Write("Please enter your name: ");
+            string name = Console.ReadLine();
+            return name;
+        }
+
+        static int PromptUserNumber()
+        {
+            Console.Write("Please enter your favorite number: ");
+            string rawNumber = Console.ReadLine();
+            
+            // Parse
+            int number = int.Parse(rawNumber);
+
+            return number;
+        }
+
+        static int SquareNumber(int number)
+        {
+            return number * number;
+        }
+
+        static void DisplayResult(string name, int squNum)
+        {
+            Console.WriteLine($"{name}, the square of your number is {squNum}");
+        }
     }
 }
