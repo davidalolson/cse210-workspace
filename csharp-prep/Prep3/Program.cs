43f9677dd03031b87fdc023ecf8a1e361753fc4d
David Olson <davidalolson>
2024-04-26 15:58:46 -0600

    completed core requirements 1-2, proceeding to core requirement 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // Variables
+        int userGuess = 0; // define init to 0
+
+        // IO
+        Console.Write("What is the magic number? ");
+        string rawMagicNumber = Console.ReadLine();
+
+        // Parse
+        int magicNumber = int.Parse(rawMagicNumber);   
+
+        // Loop
+        while (userGuess != magicNumber)
+        {
+        // IO
+            Console.Write("What is your guess? ");
+            string rawUserGuess = Console.ReadLine();
+
+            // Parse     
+            userGuess = int.Parse(rawUserGuess);
+
+            // Hint logic
+            if(magicNumber > userGuess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if(magicNumber < userGuess)
+            {
+                Console.WriteLine("Lower");
+            }
+        }
+        Console.WriteLine("You guessed it!");
     }
 }
