0721b2f984987fadfff3b17b1834eefc86d57762
David Olson <davidalolson>
2024-04-26 17:00:31 -0600

    completed all requirements and streach

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Diagnostics.CodeAnalysis;
+using System.IO.Compression;
 
 class Program
 {
@@ -10,6 +11,7 @@ class Program
 
         int sum = 0; // init to 0
         int largestNumber = 0; // init to 0
+        int smallestPosNumber = 9*10^6; // init to very large positive number (9M)
 
 
         int userInput = 1; // init to any non zero int
@@ -29,7 +31,7 @@ class Program
             numbers.Add(userInput);
         }
 
-        // Calculate sum and largest number
+        // Calculate sum, largest number, and smallest positive number
         foreach(int number in numbers)
         {
             sum += number;
@@ -37,11 +39,24 @@ class Program
             {
                 largestNumber = number;
             }
+            if (number > 0 && number < smallestPosNumber)
+            {
+                smallestPosNumber = number;
+            }
         }
-        
+        // Sort list
+        numbers.Sort();
+
         // IO
         Console.WriteLine($"The sum is: {sum}");
         Console.WriteLine($"The average is: {(double)sum/(numbers.Count-1)}"); // adding double ensures floating point value
         Console.WriteLine($"The largest number is: {largestNumber}");
+        Console.WriteLine($"The smallest positive number is: {smallestPosNumber}");
+        Console.WriteLine("The soreted list is: ");
+        
+        foreach(int number in numbers)
+        {
+            Console.WriteLine(number);
+        }
     }
 }
