68d40f96686badc12a77dad4142da46df9afeb84
David Olson <davidalolson>
2024-05-02 13:04:34 -0600

    completed step 5

---
+++ prepare/Learning02/Job.cs
@@ -0,0 +1,7 @@
+public class Job
+{
+    public string _company;
+    public string _jobTitle;
+    public string _startYear;
+    public string _endYear;
+}
--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._company = "BIOTRONIC";
+        Console.WriteLine(job1._company);
+
+        Job job2 = new Job();
+        job2._company = "MSEI";
+        Console.WriteLine(job2._company);
     }
 }
