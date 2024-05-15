3252edc52fe8dbc75d0bdfce735632ae713b9a7c
David Olson <davidalolson>
2024-05-15 14:32:02 -0600

    completed step 4

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,7 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        Assignment assignment = new Assignment("David Olson", "Multivariable Calculus");
-        Console.WriteLine(assignment.GetSummary());
+        // Assignment assignment = new Assignment("David Olson", "Multivariable Calculus");
+        // Console.WriteLine(assignment.GetSummary());
+        MathAssignment mathAssignment = new MathAssignment("David Olson", "Multivariable Calculus", "Section 3.8", "Problems 8-19");
+        Console.WriteLine(mathAssignment.GetSummary());
+        Console.WriteLine(mathAssignment.GetHomeworkList());
     }
 }
