312e3132975f31ae49a8941bf9006af93671da33
David Olson <davidalolson>
2024-05-23 13:18:21 -0600

    completed through step 5

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,8 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Square square = new Square("blue", 5);
+        Console.WriteLine(square.GetColor());
+        Console.WriteLine(square.GetArea());
     }
 }
